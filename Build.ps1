cc7e6e281769d00cd1e8407efdfa5e4e9e1e89cd
Corey Tollerud <tollerudsoftware@gmail.com>
2020-09-19 22:01:11 +0200

    Created a basic build posh script

---
+++ Build.ps1
@@ -0,0 +1,29 @@
+<#
+    Runs tests then builds the nuget package.
+#>
+$ErrorActionPreference = 'STOP'
+
+function exec {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ScriptBlock]$Script
+    )
+
+    Invoke-Command $Script
+
+    if($LASTEXITCODE -ne 0) {
+        throw "process '$Script' exited with code $LASTEXITCODE"
+    }
+}
+#####BEGIN SCRIPT#####
+$solutionPath = Join-Path $PSScriptRoot src/LinqGarden.sln
+
+
+Write-Host "Running Tests:"
+exec{dotnet test $solutionPath}
+
+$linqGardenProjectPath = Join-Path $PSScriptRoot "src/LinqGarden/LinqGarden.csproj"
+$outputPath = Join-Path $PSScriptRoot "output/LinqGarden"
+Write-Host "Building LinqGarden to dir '$outputPath'..."
+
+exec{dotnet build $linqGardenProjectPath -c Release -o $outputPath}
