acf2284c2b09e8d45fc00c75b5a16ab068996329
Corey Tollerud <coreytollerud@gmail.com>
2021-09-13 20:46:17 +0200

    Either is now contravariant

--- src/LinqGarden/Either.cs
+++ src/LinqGarden/Either.cs
@@ -4,54 +4,51 @@ using System.Text;
 
 namespace LinqGarden
 {
+    public interface Either<out TLeft, out TRight>
+    {
+        internal TLeft RawLeftValue { get; }
+
+        internal TRight RawRightValue { get; }
+
+        internal bool IsRight { get; }
+
+        TOutput To<TOutput>(Func<TLeft, TOutput> ifLeft, Func<TRight, TOutput> ifRight) =>
+            IsRight ? ifRight(RawRightValue) : ifLeft(RawLeftValue);
+    }
+
     /// <summary>
     /// Represents a value can be in two mutually exclusive states.
     /// It is either a "Left" or a "Right".
     /// </summary>
     /// <typeparam name="TLeft"></typeparam>
     /// <typeparam name="TRight"></typeparam>
-    public sealed class Either<TLeft, TRight>
+    internal record EitherImpl<TLeft, TRight>(
+        TLeft RawLeftValue,
+        TRight RawRightValue,
+        bool IsRight
+    ) : Either<TLeft,TRight>
     {
-        /// <summary>
-        /// Returns Some if this is a left, otherwise nothing.
-        /// </summary>
-        public Maybe<TLeft> LeftValue { get; }
+        TLeft Either<TLeft, TRight>.RawLeftValue => RawLeftValue;
 
-        /// <summary>
-        /// Returns Some if this is a right, otherwise nothing.
-        /// </summary>
-        public Maybe<TRight> RightValue { get; }
+        TRight Either<TLeft, TRight>.RawRightValue => RawRightValue;
 
-        /// <summary>
-        /// Only should be available internally, since it permits invalid states.
-        /// </summary>
-        /// <param name="left"></param>
-        /// <param name="right"></param>
-        internal Either(Maybe<TLeft> left, Maybe<TRight> right)
-        {
-            LeftValue = left;
-            RightValue = right;
-        }
-
-        /// <summary>
-        /// Convert to some other type.  functions need to be provided to handle if it's a left or a right.
-        /// </summary>
-        /// <typeparam name="TOutput"></typeparam>
-        /// <param name="ifLeft"></param>
-        /// <param name="ifRight"></param>
-        /// <returns></returns>
-        public TOutput To<TOutput>(Func<TLeft, TOutput> ifLeft, Func<TRight, TOutput> ifRight) =>
-            LeftValue.To<TOutput>(
-                left => ifLeft(left),
-                () => ifRight(RightValue.ValueOrDefault()));
+        bool Either<TLeft, TRight>.IsRight => IsRight;
     }
 
     public static class Either
     {
         public static Either<TLeft, TRight> Left<TLeft, TRight>(TLeft left) =>
-            new Either<TLeft, TRight>(Maybe.Some(left), default);
+#nullable disable
+            new EitherImpl<TLeft, TRight>(left, default, false );
+#nullable enable
 
         public static Either<TLeft, TRight> Right<TLeft, TRight>(TRight right) =>
-                new Either<TLeft, TRight>(default, Maybe.Some(right));
+#nullable disable
+                new EitherImpl<TLeft, TRight>(default, right, true);
+#nullable enable
+
+        public static Maybe<TRight> GetRight<TLeft, TRight>(this Either<TLeft, TRight> input) =>
+            input.IsRight ? Maybe.Some(input.RawRightValue) : Maybe.None<TRight>();
+
     }
 }
